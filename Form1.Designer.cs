d2b56063f96b66ee8e290dd792b88eafe6deba4d
vanbaongan <1551010080ngan@gmail.com>
2017-10-04 09:06:24 +0700

    Thuc hanh buoi 3

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Thuchanhbuoi3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTenLoai = new System.Windows.Forms.TextBox();
+            this.lbTenLoai = new System.Windows.Forms.Label();
+            this.txtMaLoai = new System.Windows.Forms.TextBox();
+            this.lbMaLoai = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btThem = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btSua = new System.Windows.Forms.Button();
+            this.dvgKetQua = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgKetQua)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTenLoai);
+            this.groupBox1.Controls.Add(this.lbTenLoai);
+            this.groupBox1.Controls.Add(this.txtMaLoai);
+            this.groupBox1.Controls.Add(this.lbMaLoai);
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 109);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loại sản phẩm ";
+            // 
+            // txtTenLoai
+            // 
+            this.txtTenLoai.Location = new System.Drawing.Point(63, 66);
+            this.txtTenLoai.Name = "txtTenLoai";
+            this.txtTenLoai.Size = new System.Drawing.Size(230, 20);
+            this.txtTenLoai.TabIndex = 1;
+            // 
+            // lbTenLoai
+            // 
+            this.lbTenLoai.AutoSize = true;
+            this.lbTenLoai.Location = new System.Drawing.Point(9, 69);
+            this.lbTenLoai.Name = "lbTenLoai";
+            this.lbTenLoai.Size = new System.Drawing.Size(48, 13);
+            this.lbTenLoai.TabIndex = 0;
+            this.lbTenLoai.Text = "Tên loại ";
+            // 
+            // txtMaLoai
+            // 
+            this.txtMaLoai.Location = new System.Drawing.Point(63, 30);
+            this.txtMaLoai.Name = "txtMaLoai";
+            this.txtMaLoai.Size = new System.Drawing.Size(149, 20);
+            this.txtMaLoai.TabIndex = 1;
+            // 
+            // lbMaLoai
+            // 
+            this.lbMaLoai.AutoSize = true;
+            this.lbMaLoai.Location = new System.Drawing.Point(13, 33);
+            this.lbMaLoai.Name = "lbMaLoai";
+            this.lbMaLoai.Size = new System.Drawing.Size(44, 13);
+            this.lbMaLoai.TabIndex = 0;
+            this.lbMaLoai.Text = "Mã loại ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btSua);
+            this.groupBox2.Controls.Add(this.btXoa);
+            this.groupBox2.Controls.Add(this.btThem);
+            this.groupBox2.Location = new System.Drawing.Point(10, 125);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(325, 62);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // btThem
+            // 
+            this.btThem.Location = new System.Drawing.Point(23, 18);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(74, 27);
+            this.btThem.TabIndex = 0;
+            this.btThem.Text = "Thêm";
+            this.btThem.UseVisualStyleBackColor = true;
+            // 
+            // btXoa
+            // 
+            this.btXoa.Location = new System.Drawing.Point(116, 18);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(74, 27);
+            this.btXoa.TabIndex = 0;
+            this.btXoa.Text = "Xoá";
+            this.btXoa.UseVisualStyleBackColor = true;
+            this.btXoa.Click += new System.EventHandler(this.btXoa_Click);
+            // 
+            // btSua
+            // 
+            this.btSua.Location = new System.Drawing.Point(213, 18);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(74, 27);
+            this.btSua.TabIndex = 0;
+            this.btSua.Text = "Sửa";
+            this.btSua.UseVisualStyleBackColor = true;
+            // 
+            // dvgKetQua
+            // 
+            this.dvgKetQua.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgKetQua.Location = new System.Drawing.Point(10, 193);
+            this.dvgKetQua.Name = "dvgKetQua";
+            this.dvgKetQua.Size = new System.Drawing.Size(325, 241);
+            this.dvgKetQua.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 446);
+            this.Controls.Add(this.dvgKetQua);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgKetQua)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMaLoai;
+        private System.Windows.Forms.Label lbMaLoai;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtTenLoai;
+        private System.Windows.Forms.Label lbTenLoai;
+        private System.Windows.Forms.Button btSua;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.DataGridView dvgKetQua;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Thuchanhbuoi3_1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection cn = null;
+        private SqlCommand cmd = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cnStr = "Server = . ; Database = QLBanHang ; Integrated security = true ;";
+            cn = new SqlConnection(cnStr);
+            dvgKetQua.DataSource = GetData();
+        }
+
+        private List<object> GetData()
+        {
+            Connect();
+            string sql = "SELECT * FROM LoaiSP";
+            List<object> list = new List<object>();
+            try 
+            {
+                cmd = new SqlCommand(sql, cn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                int id;
+                string name;
+
+                while (dr.Read())
+                {
+                    id = dr.GetInt32(0);
+                    name = dr.GetString(1);
+                    var prod = new
+                    {
+                        MaLoaiSP = id,
+                        TenLoaiSP = name,
+                    };
+                    list.Add(prod);
+                }
+                dr.Close();
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+            return list;
+        }
+
+        private void Connect()
+        {
+            try
+            {
+                if (cn != null && cn.State != ConnectionState.Open)
+                {
+                    cn.Open();
+                    MessageBox.Show("Ket noi thanh cong");
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
+                ;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Disconnect()
+        {
+            if (cn != null && cn.State != ConnectionState.Closed)
+            {
+                cn.Close();
+            }
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            string sql = "DELETE FROM LoaiSP WHERE MaLoaiSP = " + txtMaLoai.Text;
+            cmd = new SqlCommand(sql, cn);
+            int numberOfRows = 0 ;
+            Connect();
+            try
+            {
+                 numberOfRows = cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+            MessageBox.Show("So dong da xoa : " + numberOfRows.ToString());
+            dvgKetQua.DataSource = GetData();
+        }
+      
+    }
+}
